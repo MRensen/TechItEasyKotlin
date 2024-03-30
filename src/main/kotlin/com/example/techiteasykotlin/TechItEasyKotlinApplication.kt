8f9cd8e720409265bf3dd46b44579ec5c0e82504
MRensen <markrensen@hotmail.com>
2024-03-30 20:06:56 +0100

    init

---
+++ src/main/kotlin/com/example/techiteasykotlin/TechItEasyKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.techiteasykotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechItEasyKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<TechItEasyKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/techiteasykotlin/TechItEasyKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.techiteasykotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TechItEasyKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
